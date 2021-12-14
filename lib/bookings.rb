de2ac57917e96482caef340950e57129d6d89a23
Chris Clement <chrisclement@Chriss-Air.connect>
2021-12-14 15:19:23 +0000

    Created check_availability method and passed unit tests

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -1,7 +1,18 @@
+require 'pg'
+require 'database_connection'
 class Bookings
   def self.print_dates
     t = Time.now
     date_array = (0..6).map { |num| t + 86400 * num}
     date_array_formatted = date_array.map {|date| date.strftime("%d-%m-%Y")}
+  end
+
+  def self.check_availability(space_id, date)
+    result = DatabaseConnection.query("SELECT space_id, date FROM bookings;", [])
+    if result.count == 0
+      'Available'
+    else
+      'Not Available'
+    end
   end
 end
--- spec/bookings_spec.rb
+++ spec/bookings_spec.rb
@@ -1,10 +1,26 @@
 require 'bookings'
+require 'database_connection'
 
 describe Bookings do 
   describe '.print_dates' do
     it 'would show the next 7 days' do
       expect(Bookings.print_dates).to include Time.now.strftime("%d-%m-%Y")
       expect(Bookings.print_dates).to include (Time.now + 3 * 86400).strftime("%d-%m-%Y")
+    end
+  end
+  describe '.check_availability' do
+    context 'space is unavailable for that date' do
+      it 'returns Not Available' do
+        connection = DatabaseConnection.setup('makers_bnb_test')
+        connection.query("INSERT INTO bookings(date) VALUES ($1);", ['15-12-2021' ])
+        expect(Bookings.check_availability(1, '15-12-2021')).to eq 'Not Available'
+      end
+    end
+    context 'space is available for that date' do
+      it 'returns Available' do
+        connection = DatabaseConnection.setup('makers_bnb_test')
+        expect(Bookings.check_availability(1, '15-12-2021')).to eq 'Available'
+      end
     end
   end
 end
