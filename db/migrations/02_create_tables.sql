30150d0f66820636a3f9711e064db0515b99b4ef
Chris Clement <chrisclement@Chriss-Air.connect>
2021-12-14 12:12:04 +0000

    Tests written for seeing if dates are available

--- db/migrations/02_create_tables.sql
+++ db/migrations/02_create_tables.sql
@@ -2,4 +2,6 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(60) UNIQUE, password
 CREATE TABLE spaces (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users(id), price INTEGER, name VARCHAR(60), description VARCHAR(240));
 CREATE TABLE bookings (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users(id), space_id INTEGER REFERENCES spaces(id), date VARCHAR(60), bookable BOOLEAN);
 
+ALTER TABLE bookings DROP COLUMN bookable;
+
 This is for both makers_bnb and makers_bnb_test databases.
--- spec/features/add_booking_spec.rb
+++ spec/features/add_booking_spec.rb
@@ -1,7 +1,16 @@
-feature ' Add bookings ' do
+feature 'Add bookings' do
     scenario 'A user should be able to click on book now and see a available dates' do
         visit('/add_booking')
         click_on('Book Now')
-        expect(page).to have_content('example date 1')
+        expect(page).to have_content(Time.now.strftime("%d-%m-%Y"))
+    end
+end
+
+feature 'After clicking book now, a user sees if a date is available' do
+    scenario 'A user should be able to see if a date is available' do
+        visit('/add_booking')
+        click_on('Book Now')
+        expect(page).to have_content('Available')
+        expect(page).to have_content('Not Available')
     end
 end
