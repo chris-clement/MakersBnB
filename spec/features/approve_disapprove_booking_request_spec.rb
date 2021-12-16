583309f99d33713030670e6c0a0736afb0804db4
Chris Clement <chrisclement@Chriss-Air.connect>
2021-12-16 10:50:17 +0000

    login_and_visit_home web helper. clean up of tests to use this

--- spec/features/approve_disapprove_booking_request_spec.rb
+++ spec/features/approve_disapprove_booking_request_spec.rb
@@ -1,27 +1,19 @@
 feature 'Approve a booking request' do
   scenario 'a user requested to book a date and I approve it' do
-    DatabaseConnection.query("INSERT INTO users(username, password) VALUES('firstuser', 'password');")
-    Bookings.add_booking('18-12-2021')
-    go_to_login_page
-    fill_in('username', with: 'firstuser')
-    fill_in('password', with: 'password')
-    click_on('Login')
+    Bookings.add_booking("#{today_date}")
+    login_and_visit_home
     click_on('Check Request')
     click_on('Approve')
-    expect(page).to have_content('18-12-2021 - Approved')
+    expect(page).to have_content("#{today_date} - Approved")
   end
 end
 
 feature 'Disapprove a booking request' do
   scenario 'a user requested to book a date and I disapprove it' do
-    DatabaseConnection.query("INSERT INTO users(username, password) VALUES('firstuser', 'password');")
-    Bookings.add_booking('18-12-2021')
-    go_to_login_page
-    fill_in('username', with: 'firstuser')
-    fill_in('password', with: 'password')
-    click_on('Login')
+    Bookings.add_booking("#{today_date}")
+    login_and_visit_home
     click_on('Check Request')
     click_on('Disapprove')
-    expect(page).to have_content('18-12-2021 - Disapproved')
+    expect(page).to have_content("#{today_date} - Disapproved")
   end
 end
--- spec/features/viewing_booking_request_spec.rb
+++ spec/features/viewing_booking_request_spec.rb
@@ -1,23 +1,13 @@
 feature 'Viewing booking request' do
   scenario 'user is able to see if their space has been requested to book' do
-    DatabaseConnection.query("INSERT INTO users(username, password) VALUES('firstuser', 'password');")
-    go_to_login_page
-    fill_in('username', with: 'firstuser')
-    fill_in('password', with: 'password')
-    expect(current_path).to eq '/login'
-    click_on('Login')
+    login_and_visit_home
     click_on('Check Request')
     expect(page).to have_content('Current Booking Request:')
   end
 
   scenario 'user is able to see which space has been requested to book' do
-    DatabaseConnection.query("INSERT INTO users(username, password) VALUES('firstuser', 'password');")
     Bookings.add_booking('18-12-2021')
-    go_to_login_page
-    fill_in('username', with: 'firstuser')
-    fill_in('password', with: 'password')
-    expect(current_path).to eq '/login'
-    click_on('Login')
+    login_and_visit_home
     click_on('Check Request')
     expect(page).to have_content('London Road, 18-12-2021')
   end
